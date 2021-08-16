ba5c89437c0422106445d6b8334d1f17bb452161
juanchovelezpro <velezolaya2012@hotmail.com>
2021-08-15 23:25:40 -0500

    Knight movement done.

--- src/model/Knight.kt
+++ src/model/Knight.kt
@@ -4,9 +4,9 @@ class Knight(position: Coordinate, team: Team) : Piece(position, team) {
 
     init {
 
-        shape = if(team == Team.WHITE){
+        shape = if (team == Team.WHITE) {
             "\u2658"
-        }else{
+        } else {
             "\u265E"
         }
 
@@ -14,6 +14,115 @@ class Knight(position: Coordinate, team: Team) : Piece(position, team) {
 
     override fun possibleMovements(board: Array<Array<Box>>): ArrayList<Box> {
         val pMovements = ArrayList<Box>()
+
+        // L  -> UP LEFT
+        if (position.x - 2 >= 0 && position.y - 1 >= 0) {
+            val box = board[position.x - 2][position.y - 1]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+
+        }
+
+        // L mirror -> UP RIGHT
+        if (position.x - 2 >= 0 && position.y + 1 <= 7) {
+            val box = board[position.x - 2][position.y + 1]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+        // L -> MID UP LEFT
+        if (position.x - 1 >= 0 && position.y - 2 >= 0) {
+            val box = board[position.x - 1][position.y - 2]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+        // L -> MID UP RIGHT
+        if (position.x - 1 >= 0 && position.y + 2 <= 7) {
+            val box = board[position.x - 1][position.y + 2]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+        // L -> MID DOWN LEFT
+        if (position.x + 1 <= 7 && position.y - 2 >= 0) {
+            val box = board[position.x + 1][position.y - 2]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+        // L -> MID DOWN RIGHT
+        if (position.x + 1 <= 7 && position.y + 2 <= 7) {
+            val box = board[position.x + 1][position.y + 2]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+        // L -> LEFT DOWN
+        if(position.x + 2 <= 7 && position.y - 1 >= 0){
+            val box = board[position.x + 2][position.y - 1]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+        // L -> RIGHT DOWN
+        if(position.x + 2 <= 7 && position.y + 1 <= 7){
+            val box = board[position.x + 2][position.y + 1]
+
+            if (box.piece == null) {
+                pMovements.add(box)
+            } else {
+                if (isEnemy(box.piece!!)) {
+                    pMovements.add(box)
+                }
+            }
+        }
+
+
+
+
 
         return pMovements
     }
--- src/ui/Window.kt
+++ src/ui/Window.kt
@@ -19,14 +19,7 @@ class Window : JFrame() {
 
         chess = Chess()
 
-        chess.board[2][6].piece = Rook(Coordinate(2,6), Team.BLACK)
-        chess.board[2][3].piece = Rook(Coordinate(2,3), Team.WHITE)
-        chess.board[5][6].piece = Pawn(Coordinate(5,6), Team.BLACK)
-        chess.board[5][1].piece = Pawn(Coordinate(5,1), Team.BLACK)
-        chess.board[3][3].piece = Pawn(Coordinate(3,3), Team.BLACK)
-        chess.board[3][5].piece = Pawn(Coordinate(3,5), Team.BLACK)
-        chess.board[4][3].piece = Queen(Coordinate(4,3), Team.WHITE)
-        chess.board[4][4].piece = Queen(Coordinate(4,4), Team.BLACK)
+        chess.board[3][3].piece = Knight(Coordinate(3,3),Team.WHITE)
 
         boardPanel = BoardPanel(this)
 
