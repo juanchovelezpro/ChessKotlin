d24741b478e20984b06c99b97058e392e8debe6a
juanchovelezpro <velezolaya2012@hotmail.com>
2021-08-15 20:00:47 -0500

    Queen movement done.

--- src/model/Queen.kt
+++ src/model/Queen.kt
@@ -15,6 +15,23 @@ class Queen(position: Coordinate, team: Team) : Piece(position, team) {
     override fun possibleMovements(board: Array<Array<Box>>): ArrayList<Box> {
         val pMovements = ArrayList<Box>()
 
+        // As the Queen movements are like Rook and Bishop movements, then we can get from those classes
+
+        val rook : Rook
+        val bishop : Bishop
+
+        if(team == Team.WHITE) {
+            rook = Rook(Coordinate(position.x, position.y), Team.WHITE)
+            bishop = Bishop(Coordinate(position.x,position.y), Team.WHITE)
+        }else{
+            rook = Rook(Coordinate(position.x, position.y), Team.BLACK)
+            bishop = Bishop(Coordinate(position.x,position.y), Team.BLACK)
+        }
+
+        pMovements.addAll(rook.possibleMovements(board))
+        pMovements.addAll(bishop.possibleMovements(board))
+
+
         return pMovements
     }
 
--- src/ui/Window.kt
+++ src/ui/Window.kt
@@ -25,7 +25,8 @@ class Window : JFrame() {
         chess.board[5][1].piece = Pawn(Coordinate(5,1), Team.BLACK)
         chess.board[3][3].piece = Pawn(Coordinate(3,3), Team.BLACK)
         chess.board[3][5].piece = Pawn(Coordinate(3,5), Team.BLACK)
-        chess.board[4][3].piece = Bishop(Coordinate(4,3), Team.WHITE)
+        chess.board[4][3].piece = Queen(Coordinate(4,3), Team.WHITE)
+        chess.board[4][4].piece = Queen(Coordinate(4,4), Team.BLACK)
 
         boardPanel = BoardPanel(this)
 
