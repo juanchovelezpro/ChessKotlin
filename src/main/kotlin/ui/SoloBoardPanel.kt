d2485c9f081cbf3e1a6bcb485097a852345c2cb8
juanchovelezpro <velezolaya2012@hotmail.com>
2022-01-16 19:18:37 -0500

    AI can choose a piece on promotion

--- src/main/kotlin/ui/SoloBoardPanel.kt
+++ src/main/kotlin/ui/SoloBoardPanel.kt
@@ -1,11 +1,28 @@
 package ui
 
-import model.AIChess
-import model.ChessActions
-import model.Team
+import model.*
+import kotlin.random.Random
 
 class SoloBoardPanel(window: Window) : BoardPanel(window), ChessActions {
 
     val AI = AIChess(Team.BLACK, window.chess)
+
+    override fun onPromotion(pawn: Pawn, promPosition: Coordinate): Piece {
+
+        return if(pawn.team == Team.BLACK){
+
+            val options = arrayOf("Rook", "Bishop", "Knight", "Queen")
+
+            when (options[Random.nextInt(0, options.lastIndex)]) {
+                "Rook" -> Rook(promPosition, pawn.team, pawn.observer)
+                "Bishop" -> Bishop(promPosition, pawn.team, pawn.observer)
+                "Knight" -> Knight(promPosition, pawn.team, pawn.observer)
+                else -> Queen(promPosition, pawn.team, pawn.observer)
+            }
+        }else {
+            super.onPromotion(pawn, promPosition)
+        }
+    }
+
 
 }
